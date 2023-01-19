a4facf02d51a2b2c1b16d8641389dd2cf245db08
Admin <admin@Admins-MacBook-Pro.local>
2023-01-19 00:43:59 -0600

    finished first part of navigation bar, and started last part of footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,25 @@
 const Footer = () => {
     return (
       <footer>
-        Copyright 2023 ACM Entrepreneurship
+        <section className='join-mailing-list'>
+          <div className='mailing-list'>
+            <form>
+              <input type='email' name='email' placeholder='enter your email and press Enter' />
+            </form>
+          </div>
+        </section>
+        <section className='footer-information'>
+          <ul>
+            <li><a>About our E-commerce company</a></li>
+            <li><a>About ACM Entrepreneurship</a></li>
+            <li><a>About MST ACM</a></li>
+            <li><a>Contact</a></li>
+            <li><a>Terms of Service</a></li>
+          </ul>
+        </section>
+        <section className='copyright'>
+          Copyright 2023 ACM Entrepreneurship
+        </section>
       </footer>
     );
   }
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,30 +2,44 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 
+
 const Navbar = () => {
   return (
-    <nav>
-        <div className="nav-belt">
-            <div className="site-logo">
-                <Image src="/site-logo.png" alt="site logo" width={140} height={120} />
+      <nav>
+        <div className='nav-belt'>
+            <div className='site-logo'>
+                <Image src='/site-logo.png' alt='site logo' width={140} height={120} />
             </div>
             <div className='nav-belt-button'>
-                <Link href="/">Home - Search bar holder</Link>
-                <Link href="/about">About - Sign in/Sign up holder</Link>
-                <Link href="/about">About - Cart holder</Link>
+                <ul>
+                    <li>
+                        <div className='navbar-search'>
+                            <ul>
+                                <li>
+                                    <form>
+                                        <input type='text' name='main-bar-search' placeholder='search' />
+                                    </form>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div className='nav-signin'>
+                            <Image src='/signin-icon.png' alt='main signin icon' width={30} height={30} />
+                            <a href='/'>Welcome<br />Sign in/Sign up</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div className='nav-cart'>
+                            <a href='/'>
+                                <Image src='/shopping-cart.png' alt='main shopping cart icon' width={60} height={60} />
+                            </a>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
-        <div className="nav-main">
-            <div className="nav-left">
-            </div>
-            <div className='nav-vendor'>
-                <Link href="/">All</Link>
-                <Link href="/about">ACM-E</Link>
-                <Link href="/about">NSBE</Link>
-                <Link href="/about">SHPE</Link>
-            </div>
-        </div>
-    </nav>
+      </nav>
   );
 }
 
