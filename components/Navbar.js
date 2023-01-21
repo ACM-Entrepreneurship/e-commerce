5c0d754fd117fb933887cf041b1ce0ef439aaf77
Admin <admin@Admins-MacBook-Pro.local>
2023-01-21 00:33:01 -0600

    finished last part of navbar (sidebar) - basic navbar and footer done

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,31 @@
 import Link from 'next/link'
 import Image from 'next/image'
+//import openSideNav from "components/Sidenavbar"
 
+const openSideNav = () => {
+    return (
+        (document.getElementById("close-burger").style.marginLeft = "260px")
+        && (document.getElementById("nav-side-menu").style.left = "0")
+     );
+ }
 
+ const closeSideNav = () => {
+    return (
+        (document.getElementById("nav-side-menu").style.left = "-250px")
+        && (document.getElementById("close-burger").style.marginLeft = "-50px")
+     );
+ }
 
 const Navbar = () => {
   return (
-      <nav>
+    <nav>
         <div className='nav-belt'>
             <div className='site-logo'>
                 <Image src='/site-logo.png' alt='site logo' width={140} height={120} />
+            </div>
+            <div className='close-burger' id='close-burger' onClick={()=>closeSideNav()}>
+                <span></span>
+                <span></span>
             </div>
             <div className='nav-belt-button'>
                 <ul>
@@ -39,7 +56,35 @@ const Navbar = () => {
                 </ul>
             </div>
         </div>
-      </nav>
+        <div className='nav-main'>
+                <div className='nav-main-option'>
+                    <ul>
+                        <li>
+                            <div className='burger' onClick={()=>openSideNav()}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </li>
+                        <li><a href='/'>ALL</a></li>
+                        <li><a href='/'>ACM-E</a></li>
+                        <li><a href='/'>NSBE</a></li>
+                        <li><a href='/'>SHPE</a></li>
+                    </ul>
+                </div>
+        </div>
+        <div id='nav-side-menu' className='nav-side-menu'>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Category</a></li>
+                <li><a href="/">All vendors</a></li>
+                <li><a href="/">Best sellers</a></li>
+                <li><a href="/">Deals</a></li>
+                <li><a href="/">About</a></li>
+            </ul>
+        </div>
+        
+    </nav>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,24 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Layout from '@/components/Layout'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
   return (
-    <>
+    <> 
+      
       <Layout>
       <Head>
         <title>ACM-E E-commerce</title>
         <meta name="description" content="E-commerce" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
+      <div>s </div>
+      <div>s</div>
+      <div>s</div>
       <div>Lorem Ipsum</div>
       <div>Lorem Ipsum</div>
       <div>Lorem Ipsum</div>
