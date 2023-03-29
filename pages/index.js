deddd30044d2729c1b97f71e564124fb4289cb8e
Rueil Manzambi <53281957+rueilmanzambi@users.noreply.github.com>
2023-03-29 01:10:14 -0500

    added first homepage gallery

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
-
 export default function Home() {
+
   return (
     <> 
       
@@ -18,72 +18,195 @@ export default function Home() {
         <meta name="description" content="E-commerce" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <div>s </div>
-      <div>s</div>
-      <div>s</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
-      <div>Lorem Ipsum</div>
+      <div class={styles.bodydiv}>
+        <section class={styles.product}>
+          <button  class={styles.prebtn}><Image src="/arrow.png" width={20} height={30} alt="" onClick={() => {document.getElementById("firstproductcontainer").scrollLeft -= 500}}/></button>
+          <button  class={styles.nxtbtn}><Image src="/arrow.png" width={20} height={30} alt="" onClick={() => {document.getElementById("firstproductcontainer").scrollLeft += 500}}/></button>
+            <div class={styles.productcontainer} id='firstproductcontainer'>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                        <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$20</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                      <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$20</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                      <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$20</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                      <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$20</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                      <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$20</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                      <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$20</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                      <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$20</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                      <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$25</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                      <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$25</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+                <div class={styles.productcard}>
+                    <div class={styles.productimage}>
+                      <Image src="/site-logo.png" class={styles.productthumb} width={250} height={450} alt="" />
+                        <button class={styles.cardbtn}>add to wishlist</button>
+                    </div>
+                    <div class={styles.productinfo}>
+                        <h2 class={styles.productbrand}>brand</h2>
+                        <p class={styles.productshortdescription}>a short line about the cloth..</p>
+                        <span class={styles.price}>$25</span><span class={styles.actualprice}>$40</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class={styles.productsgallery}>
+          <div>s </div>
+          <div>s</div>
+          <div>sb</div>
+        </div>
+        <div>s </div>
+        <div>s</div>
+        <div>sb</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+        <div>Lorem Ipsum</div>
+      </div>
       </Layout>
     </>
   )
