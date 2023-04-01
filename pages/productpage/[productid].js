843dc3fb841e1a80d205137a40a479967887266b
Rueil Manzambi <53281957+rueilmanzambi@users.noreply.github.com>
2023-04-01 15:55:53 -0500

    saving changes before merging dev branch to this one

--- pages/productpage/[productid].js
+++ pages/productpage/[productid].js
@@ -1,46 +1,69 @@
-import { useRouter } from 'next/router'
-import prisma from '../../lib/prisma'
-import { gql, useQuery } from '@apollo/client'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../../styles/template-product-page.module.css';
+import Layout from '../../components/Layout';
+import Navbar from '../../components/Navbar';
+import Link from 'next/link';
 
-
-function ProductPage(props) {
-    const router = useRouter();
-    
-    const uniqueproduct = props.uniqueproduct;
-    
+export default function ProductPage(){
     return (
-        <div>
-            <ul>
-                <li key={uniqueproduct.id}>
-                    <img src={uniqueproduct.imageUrl} />
-                    <div>
-                        <p>{uniqueproduct.category}</p>
-                        <p>{uniqueproduct.name}</p>
-                        <p>{uniqueproduct.description}</p>
-                        <p>{uniqueproduct.manufacturer}</p>
-                        <p>{uniqueproduct.price}</p>
-                        <a href={uniqueproduct.url}>
-                            {uniqueproduct.url.replace(/(^\w+:|^)\/\//, '')}
-                        </a>
+        <>
+        <Layout>
+        <Head>
+        <title>MinerWarehouse - Product Template</title>
+        <meta name="description" content="E-commerce" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+            <div class={styles.row1}>
+                <div class={styles.column_1}>
+                    <ul class={styles.productImages}>
+                        <li class={styles.Image}>Image 1</li>
+                        <li class={styles.Image}>Image 2</li>
+                        <li class={styles.Image}>Image 3</li>
+                    </ul>
+                </div>
+                <div class={styles.column_2}>
+                    <h1>main image</h1>
+                </div>
+                <div class={styles.column_3}>
+                    <h1 id={styles.prodTitle}>Product Title</h1>
+                    <div class={styles.topBorderLine}></div>
+                    <h1 class={styles.prodPrice}>$0.00</h1>
+                    <div class={styles.prodInfoLayout}>
+                        <h3>Product Description: </h3>
+                        <ul class={styles.productDescipList}>
+                            <li class={styles.productInfoPt}>Info 1</li>
+                            <li class={styles.productInfoPt}>Info 2</li>
+                            <li class={styles.productInfoPt}>Info 3</li>
+                        </ul>
                     </div>
-                </li>     
-            </ul>
-        </div>
+                    <div class={styles.topBorderLine2}></div>
+                </div>
+                <div class={styles.column_4}>
+                    <h1 class={styles.prodPrice}>$0.00</h1>
+                    <h3 class={styles.stockInfo}>In stock</h3>
+                    <button class={styles.cartBtn}>ADD TO CART</button>
+                    <button class={styles.buyBtn}>BUY NOW</button>
+                </div>
+                <div class={styles.clr}></div>
+            </div>
+
+
+            
+            <div class={styles.productInfo}>
+                <h1>Product Information</h1>
+                <ul>
+                    <li>Product Dimension: </li>
+                    <li>Item Weight: </li>
+                    <li>ACM-E SIN: </li>
+                    <li>Item Model Number: </li>
+                    <li>Manufacturer: </li>
+                    <li>Customer Reviews: </li>
+                    <li>Date First Available: </li>
+                </ul>
+            </div>
+        </Layout>
+        </>
     )
 }
-
-export async function getServerSideProps(context) {
-    const {productid} = context.query;
-    const product = await prisma.product.findUnique({
-        where: {
-            id : productid,
-        },
-    });
-    return {
-        props: {
-            uniqueproduct: JSON.parse(JSON.stringify(product)),
-        }
-    }
-}
-
-export default ProductPage;
